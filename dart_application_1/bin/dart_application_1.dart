32f70bf73c0d97a2e09dc32e86e35a376230fea2
irvanln <irvina22@gmail.com>
2023-01-17 22:58:18 +0700

    variable dan tipe data

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,14 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  //declaring variable
+  var kota = "Jakarta";
+
+  String nama;
+
+  nama = "Irvan";
+
+  print('Hello world:' + kota);
+  print(nama);
+}
