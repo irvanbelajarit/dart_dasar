1cf2b9ef67b55a5ae0b9be7b00853a1d96fc31fe
irvanln <irvina22@gmail.com>
2023-01-25 06:37:33 +0700

    Switch Case

--- dart_application_selection/bin/dart_application_selection.dart
+++ dart_application_selection/bin/dart_application_selection.dart
@@ -20,4 +20,48 @@ void main(List<String> arguments) {
   } else {
     print("Jika salah Tampilkan Statement ini");
   }
+
+  print("\nPerintah seleksi Switch Case");
+  int age = 25;
+  switch (age) {
+    case 18:
+      print('You are an adult');
+      break;
+    case 25:
+      print('You are in your prime');
+      break;
+    case 50:
+      print('You are middle-aged');
+      break;
+    default:
+      print('Invalid age');
+  }
+
+  print("\nSwitch case 2");
+  String name = "John";
+  switch (name) {
+    case "John":
+      print('Hello John');
+      break;
+    case "Jane":
+      print('Hello Jane');
+      break;
+    default:
+      print('Invalid name');
+  }
+
+  print("\nJika tidak menggunakan perintah Break pada setiap case");
+  int month = 2;
+  switch (month) {
+    case 12:
+    case 1:
+    case 2:
+      print('Winter');
+      break;
+    case 3:
+    case 4:
+    case 5:
+      print('Spring');
+      break;
+  }
 }
